fd9dc21474d6ca6cfda93160ece66697bca1152a
NaikSatvik <satvikdnaik@gmail.com>
2022-09-23 19:49:05 +0530

    Patterns Done.

---
+++ Patterns/FloydsTriangle.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+class FloydsTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int c = 1;
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=i;j++) {
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
